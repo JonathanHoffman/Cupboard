612fc6089cba565eed2eca501cda2e32a870b280
Jonathan Hoffman <jonathanghoffman@gmail.com>
2017-01-02 10:31:56 -0600

    Support deleting of ingredients

--- Cupboard/Cupboard/EnterIngredientsTableViewController.swift
+++ Cupboard/Cupboard/EnterIngredientsTableViewController.swift
@@ -88,26 +88,24 @@ class EnterIngredientsTableViewController: UITableViewController, UITextFieldDel
         }
     }
     
-    
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
+        // Do not allow editing of the ingredient entry cell
+        if indexPath.section == 0 {
+            return false
+        }
         return true
     }
-    */
 
-    /*
-    // Override to support editing the table view.
+    // Support for editing of the table view
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            ingredients.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // MARK: - Navigation
